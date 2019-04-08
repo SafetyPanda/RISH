dffb5b79d913e38d4bae9f178859cbd7de16caa9
James Gillman <jronaldgillman@gmail.com>
2019-04-07 23:44:30 -0600

    Proper Ruby names and commenting set_config

--- customization.rb
+++ customization.rb
@@ -17,20 +17,20 @@ class String
     def gray;           "\e[37m#{self}\e[0m" end
     
     #Change Background
-    def bgBlack;       "\e[40m#{self}\e[0m" end
-    def bgRed;         "\e[41m#{self}\e[0m" end
-    def bgGreen;       "\e[42m#{self}\e[0m" end
-    def bgBrown;       "\e[43m#{self}\e[0m" end
-    def bgBlue;        "\e[44m#{self}\e[0m" end
-    def bgMagenta;     "\e[45m#{self}\e[0m" end
-    def bgCyan;        "\e[46m#{self}\e[0m" end
-    def bgGray;        "\e[47m#{self}\e[0m" end
+    def bg_Black;       "\e[40m#{self}\e[0m" end
+    def bg_Red;         "\e[41m#{self}\e[0m" end
+    def bg_Green;       "\e[42m#{self}\e[0m" end
+    def bg_Brown;       "\e[43m#{self}\e[0m" end
+    def bg_Blue;        "\e[44m#{self}\e[0m" end
+    def bg_Magenta;     "\e[45m#{self}\e[0m" end
+    def bg_Cyan;        "\e[46m#{self}\e[0m" end
+    def bg_Gray;        "\e[47m#{self}\e[0m" end
     
     # Formatting
     def bold;           "\e[1m#{self}\e[22m" end
     def italic;         "\e[3m#{self}\e[23m" end
     def underline;      "\e[4m#{self}\e[24m" end
-    def reverseColor;  "\e[7m#{self}\e[27m" end
+    def reversecolor;  "\e[7m#{self}\e[27m" end
 end
 
 def set_config()
